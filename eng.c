bce8dea234b93bb8bf675b9798201d85e2497079
Wabochi <160873180+Wabochi@users.noreply.github.com>
2024-02-22 17:40:46 +0300

    Create eng.c

---
+++ eng.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+//#include<time.h>
+#include<unistd.h>
+
+int main(int argc, char *argv[])
+{
+	
+while(1==1){
+	
+
+char *command = "portal > /dev/null 2>&1";
+
+system(command);
+sleep(12);
+
+}
+	
+	
+}
+
